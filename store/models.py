f0b3621121cc997a9b0eba68edb25c508a667afa
Yuuki1602 <kakashi16023@gmail.com>
2022-03-19 20:27:45 +0700

    cn

---
+++ store/models.py
@@ -0,0 +1,29 @@
+from pyexpat import model
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE, null=True,blank=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    price =models.FloatField(default=False, null=True, blank=False)
+    digital = models.BooleanField(default=False, null=True, blank=False)
+    image = models.ImageField(null=True, blank =True)
+
+    def __str__(self):
+        return self.name
+        
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url =''
+        return url
